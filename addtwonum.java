e24d920cde7f09633a414244b9f6ad215d54b3a6
Yashas D P <dpyash7@gmail.com>
2026-09-24 21:10:57 +0530

    Added addition operation by two numbers

---
+++ addtwonum.java
@@ -0,0 +1,9 @@
+package javalearning;
+
+public class addtwonum {
+    public static void main(String args[]){
+        int num1=8,num2=10;
+        int result=num1+num2;
+        System.out.println(result);
+    }
+}
